c6b07063dd511cc643c40c18ae0ac2b6e7afc649
Wali Ullah <wali190939@gmail.com>
2024-08-19 21:04:39 +0600

    add: noemon

--- src/index.ts
+++ src/index.ts
@@ -124,5 +124,5 @@ app
 const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
-  console.log(`Backend app listening at http://localhost:${port}`);
+  console.log(`Backend app listening at ${port}`);
 });
